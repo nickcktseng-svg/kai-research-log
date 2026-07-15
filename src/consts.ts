fa0e1d198d347c5a82ca5915c9101db3aeb82458
nickcktseng-svg <nick.cktseng@gmail.com>
2026-07-16 03:14:31 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '曾智凱 Kai｜計算化學研究日誌';
+
+export const SITE_DESCRIPTION =
+	'記錄計算化學、VASP、DFT、HPC、模型建構、文獻整理與研究工具開發的個人研究網站。';
